4a01da3923ed8d5301f446cd6950b3ee176939dc
Rafael Degolin <rafaeldegolin26@hotmail.com>
2022-11-17 10:10:43 -0300

    Tela de login e registro funcionando (mas ainda sem comunicar com o banco)

--- app/src/main/java/com/OverhealthApp/TelaLogin.java
+++ app/src/main/java/com/OverhealthApp/TelaLogin.java
@@ -2,7 +2,10 @@ package com.OverhealthApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TelaLogin extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class TelaLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
+        TextView bt = findViewById(R.id.textViewRegistro);
+        bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TelaLogin.this, TelaRegistro.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/OverhealthApp/TelaRegistro.java
+++ app/src/main/java/com/OverhealthApp/TelaRegistro.java
@@ -2,7 +2,10 @@ package com.OverhealthApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class TelaRegistro extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class TelaRegistro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_registro);
+
+        TextView bt = findViewById(R.id.jaTemConta);
+        bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TelaRegistro.this, TelaLogin.class));
+            }
+        });
     }
 }
